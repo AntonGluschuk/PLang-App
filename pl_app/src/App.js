cb602feb7705214bb57dc55b00f5b3d1e24918a0
Anton Gluschuk <32397351+AntonGluschuk@users.noreply.github.com>
2020-09-30 11:57:42 +0300

    trying react router and scss

--- pl_app/src/App.js
+++ pl_app/src/App.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import DataTypes from './components/DataTypes/DataTypes';
+import Home from './components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link  
+} from 'react-router-dom';
 
 function App() {
-  return <div className="App">Hello World!</div>;  
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/data_types'>Data Types</Link> 
+            </li>
+          </ul>
+        </nav>
+        <Switch>          
+        <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/data_types'>
+            <DataTypes/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ pl_app/src/components/DataTypes/DataTypes.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import './style/DataTypes.css';
+
+function DataTypes() {
+    return (
+        <div>
+          <div className='dt__heading'>
+            <h1>Data Types</h1>
+          </div>
+
+          <div className='dt_container'>
+
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>01</h2>
+                <h1>Strings</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>02</h2>
+                <h1>Numbers</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>03</h2>
+                <h1>Booleans</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+
+  
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>04</h2>
+                <h1>BigInt</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+  
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>05</h2>
+                <h1>Object</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+  
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>06</h2>
+                <h1>Symbol</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+
+  
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>07</h2>
+                <h1>Undefined</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+
+  
+            <div className='dt__card'>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div className='dt__card_content'>
+                <h2>08</h2>
+                <h1>Null</h1>              
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi neque molestiae eius explicabo mollitia tempore reiciendis, vel, sequi.
+                </p>
+              </div>
+            </div>
+                      
+          </div>
+        </div>   
+    )
+}
+
+export default DataTypes;
